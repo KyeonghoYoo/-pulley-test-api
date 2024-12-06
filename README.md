996eb86bf4c20c28b3dcbe1d28b2398454bfba6c
유경호 <ykh6242@gmail.com>
2024-12-06 20:28:17 +0900

    init project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# -pulley-test-api
-프리윌린 풀리팀 백엔드 개발자 채용 과제 전형 건
+# pulley-test-api
+
+백엔드 개발자 채용 과제 전형 건
---
+++ src/main/kotlin/me/kyeong/pulleytestapi/PulleyTestApiApplication.kt
@@ -0,0 +1,11 @@
+package me.kyeong.pulleytestapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PulleyTestApiApplication
+
+fun main(args: Array<String>) {
+      runApplication<PulleyTestApiApplication>(*args)
+}
